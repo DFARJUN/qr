52499c193aa2efb9046cbbe2243e980b6d1b66b1
DFARJUN <49616584+DFARJUN@users.noreply.github.com>
2021-11-21 14:10:56 +0200

    Update try.js

--- try.js
+++ try.js
@@ -1,10 +1,11 @@
   
-  function myFunction(){
+  function listenQr(){
   const N = document.querySelector("#print-input");
     setInterval(function(){ document.querySelector("#print-input").focus(); }, 1000);
     document.querySelector("#print-input").addEventListener('keyup', logKey);
   }
-  myFunction()
+document.addEventListener("DOMContentLoaded", () => listenQr());
+
 
 function logKey(e){
   if(e.key=="Enter"){
