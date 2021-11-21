4cea89ed59a5303dbc829f53a85f9400cf6de43e
DFARJUN <49616584+DFARJUN@users.noreply.github.com>
2021-11-21 14:12:38 +0200

    Update try.js

--- try.js
+++ try.js
@@ -1,6 +1,5 @@
   
   function listenQr(){
-  const N = document.querySelector("#print-input");
     setInterval(function(){ document.querySelector("#print-input").focus(); }, 1000);
     document.querySelector("#print-input").addEventListener('keyup', logKey);
   }
@@ -9,8 +8,8 @@ document.addEventListener("DOMContentLoaded", () => listenQr());
 
 function logKey(e){
   if(e.key=="Enter"){
-    window.open(N.value,'_self');
-    N.value="";
-    N.focus();
+    window.open(document.querySelector("#print-input").value,'_self');
+    document.querySelector("#print-input").value="";
+    document.querySelector("#print-input").focus();
   }
 }
