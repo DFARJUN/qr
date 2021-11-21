f1ede19bd746cee0bcea071e21c4cf10663ba985
DFARJUN <49616584+DFARJUN@users.noreply.github.com>
2021-11-21 14:05:39 +0200

    Update try.js

--- try.js
+++ try.js
@@ -4,7 +4,8 @@
     setInterval(function(){ document.querySelector("#print-input").focus(); }, 1000);
     document.querySelector("#print-input").addEventListener('keyup', logKey);
   }
-  
+  myFunction()
+
 function logKey(e){
   if(e.key=="Enter"){
     indow.open(N.value,'_self');
